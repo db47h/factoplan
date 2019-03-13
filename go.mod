ae4440906460f249cc8e24f3065fa8ef70076bd5
Denis Bernard <db047h@gmail.com>
2019-03-13 17:04:18 +0100

    All data auto-generated from factorio game files

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/db47h/factor
+module github.com/db47h/factoplan
 
 require github.com/pkg/errors v0.8.1
--- importer.lua
+++ importer.lua
@@ -35,17 +35,193 @@ end
 
 require("data")
 
+-- hard-coded pseudo factories and recipes
+
+data.raw["recipe"]["coal"] = {
+    type = "recipe",
+    name = "coal",
+    energy_required = 1,
+    category = "basic-solid",
+    results = {{"coal", 1}},
+}
+data.raw["recipe"]["copper-ore"] = {
+    type = "recipe",
+    name = "copper-ore",
+    energy_required = 1,
+    category = "basic-solid",
+    results = {{"copper-ore", 1}},
+}
+data.raw["recipe"]["iron-ore"] = {
+    type = "recipe",
+    name = "iron-ore",
+    energy_required = 1,
+    category = "basic-solid",
+    results = {{"iron-ore", 1}},
+}
+data.raw["recipe"]["stone"] = {
+    type = "recipe",
+    name = "stone",
+    energy_required = 1,
+    category = "basic-solid",
+    results = {{"stone", 1}},
+}
+
+-- TODO: pseudo-items for research have weighted amounts for military and production science packs
+-- based on guesstimates for end-game infinit research.
+-- These certainly need to be refined base on data extracted from the game tech tree. Also see
+-- https://wiki.factorio.com/Technologies
+data.raw["assembling-machine"]["lab"] = {
+    type = "lab",
+    name = "lab",
+    crafting_speed = data.raw["lab"]["lab"]["researching_speed"],
+    crafting_categories = { "research" },
+}
+data.raw["recipe"]["automation-science"] = {
+    type = "recipe",
+    name = "automation-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+    },
+    results = {{"automation-science", 1}},
+}
+data.raw["recipe"]["logistic-science"] = {
+    type = "recipe",
+    name = "logistic-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+    },
+    results = {{"logistic-science", 1}},
+}
+data.raw["recipe"]["military-science"] = {
+    type = "recipe",
+    name = "military-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 0.8},
+    },
+    results = {{"military-science", 1}},
+}
+data.raw["recipe"]["chemical-science"] = {
+    type = "recipe",
+    name = "chemical-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 0.8},
+        {"chemical-science-pack", 1},
+    },
+    results = {{"chemical-science", 1}},
+}
+data.raw["recipe"]["production-science"] = {
+    type = "recipe",
+    name = "production-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 0.8},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 0.4},
+    },
+    results = {{"production-science", 1}},
+}
+data.raw["recipe"]["utility-science"] = {
+    type = "recipe",
+    name = "utility-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 0.8},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 0.4},
+        {"utility-science-pack", 1},
+    },
+    results = {{"utility-science", 1}},
+}
+data.raw["recipe"]["space-science"] = {
+    type = "recipe",
+    name = "space-science",
+    energy_required = 30,
+    category = "research",
+    ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+        {"military-science-pack", 0.8},
+        {"chemical-science-pack", 1},
+        {"production-science-pack", 0.4},
+        {"utility-science-pack", 1},
+        {"space-science-pack", 1},
+    },
+    results = {{"space-science", 1}},
+}
+
+-- hack for space science packs
+-- TODO: find a better way for this. See https://wiki.factorio.com/Rocket_silo
+if data.raw["recipe"]["rocket-part"]["result"] ~= nil then
+    r =  data.raw["recipe"]["rocket-part"]
+    r.results = {{name = r.result, amount = r.result_count or 1}}
+    data.raw["recipe"]["rocket-part"]["result"] = nil
+end
+table.insert(data.raw["recipe"]["rocket-part"]["results"], 
+    {"space-science-pack", 10})
+
+
+
+-- remove escape-pod-assembler
+
+data.raw["assembling-machine"]["escape-pod-assembler"] = nil
+
+-- remove recipes that generate the same products
+-- TODO: handle this and return an optimized prodution plan
+
+data.raw["recipe"]["basic-oil-processing"] = nil
+data.raw["recipe"]["coal-liquefaction"] = nil
+data.raw["recipe"]["heavy-oil-cracking"] = nil
+data.raw["recipe"]["light-oil-cracking"] = nil
+data.raw["recipe"]["solid-fuel-from-petroleum-gas"] = nil
+data.raw["recipe"]["solid-fuel-from-heavy-oil"] = nil
+data.raw["recipe"]["kovarex-enrichment-process"] = nil
+data.raw["recipe"]["nuclear-fuel-reprocessing"] = nil
+
+-- import done. GO!
+
 function writeFactories(types)
-    io.write("var factories = map[string]*Factory{\n")
-    for type, prop in pairs(types) do
+    io.write("var factories = []Factory{\n")
+    for type, props in pairs(types) do
         for mn, m in pairs(data.raw[type]) do 
-            io.write('\t"', mn, '": &Factory{ID: "', mn, '\", Speed: ', m[prop], ' },\n')
+            io.write('\t{ID: "', mn, '\", Speed: ', m[props.speed], ', Categories: []string{')
+            first = true
+            for i, cat in ipairs(m[props.categories]) do
+                if not first then 
+                    io.write(', ')
+                else
+                    first = false
+                end
+                io.write('"', cat, '"')
+            end
+            io.write('}},\n')
         end
     end
-    io.write("}\n\n")
+    io.write('}\n\n')
 end
 
 io.output("data.go")
+
+-- header
+
 io.write([[
 // generated by factorio importer.lua; DO NOT EDIT
 
@@ -53,15 +229,14 @@ package main
 
 ]])
 
-writeFactories({
-        ["mining-drill"] = "mining_speed",
-        ["furnace"] = "crafting_speed",
-        ["assembling-machine"] = "crafting_speed",
-        ["lab"] = "researching_speed",
-        ["offshore-pump"] = "pumping_speed"
-    })
+-- factories 
 
-io.close()
+writeFactories({
+        ["mining-drill"] = {speed = "mining_speed", categories = "resource_categories"},
+        ["furnace"] = {speed = "crafting_speed", categories = "crafting_categories"},
+        ["assembling-machine"] = {speed = "crafting_speed", categories = "crafting_categories"},
+        ["rocket-silo"] = {speed = "crafting_speed", categories = "crafting_categories"},
+    })
 
 -- for rn, r in pairs(data.raw["resource"]) do 
 --     print(rn)
@@ -79,3 +254,45 @@ io.close()
 --         end
 --     end     
 -- end
+
+io.write('var recipes = []Recipe{\n')
+
+for name, r in pairs(data.raw["recipe"]) do
+    if r.normal then
+        -- overlay normal cost parameters over root recipe
+        for k, v in pairs(r.normal) do
+            r[k] = v
+        end
+    end
+    if r.result ~= nil then
+        if r.results ~= nil then
+            print("result conflict for " .. name)
+        end
+        r.results = { {r.result, r.result_count or 1} }
+    end
+    io.write('\t{\n\t\tID: "', name, '",\n')
+    io.write('\t\tTime: ', r.energy_required or 0.5, ',\n')
+    io.write('\t\tCategory: "', r.category or "basic-crafting", '",\n')
+    
+    if r.ingredients ~= nil then
+        io.write('\t\tIngredients: []Item{\n')
+        for i, ig in ipairs(r.ingredients) do
+            io.write('\t\t\t{Name: "', ig.name or ig[1],
+                '", Amount: ', ig.amount or ig[2], '},\n')
+        end
+        io.write('\t\t},\n')
+    end
+
+    io.write('\t\tResults: []Item{\n')
+    for i, res in ipairs(r.results) do
+        io.write('\t\t\t{Name: "', res.name or res[1],
+            '", Amount: ', (res.amount or res[2]) * (res.probability or 1), '},\n')
+    end
+    io.write('\t\t},\n')
+    
+    io.write('\t},\n')
+end
+
+io.write('}\n')
+
+io.close()
